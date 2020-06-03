584c0b66f31f4b1bc2b45f1268a6c12734a22c61
eNobreg <ezra.nobrega@outlook.com>
2020-06-03 10:25:17 -0400

    [Update] Added models for the Database through Django, added main config line to settings, and .gitignore for .pyc files

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=40)
+    type = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    username = models.CharField(max_length=40)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipes(models.Model):
+    name = models.CharField(max_length=40)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.name
