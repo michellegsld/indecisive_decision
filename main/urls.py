84c87ec6b0b43174320b7ba68b0b25c3b488217b
Michelle Giraldo <michellegsld@gmail.com>
2020-06-04 14:35:55 -0400

    [Update] Add regular index, login, and signup URLs

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.home, name="home")        
+path("", views.index, name="index"),
+path("login", views.login_signup, name="login_signup"),
+path("signup", views.login_signup, name="login_signup")
 ]
