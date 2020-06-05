dcb19f3415b8f8e7a2943900e52b092e58300da4
Michelle Giraldo <michellegsld@gmail.com>
2020-06-05 19:24:02 -0400

    [Initial] Combine header.js into here as accounts for modal signup/login

---
+++ main/static/js/common.js
@@ -0,0 +1,18 @@
+window.onload = () => {
+  // Create random message in header
+  $('.header_left_text').text('*Random Message*');
+
+  // To shorten code, refers to modal
+  var modal = document.getElementById('modal');
+
+  //Display modal when "Log In/Sign Up" is clicked on
+  $('.login_signup_text').click(function () { modal.style.display = 'block'; });
+
+  //Hide modal when the 'x' is clicked on the modal
+  $('span').click(function () { modal.style.display = 'none'; });
+
+  //Hide modal when clicked on window outside of it
+  window.onclick = function (event) {
+    if (event.target === modal) { modal.style.display = 'none'; }
+  };
+};
