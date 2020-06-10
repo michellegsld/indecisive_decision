7afc24b482de6e282882f2f16d0036c00cc09c78
eNobreg <ezra.nobrega@outlook.com>
2020-06-10 15:08:46 -0400

    [Update] Base HTML: Swapped where additional statements were imported URL.py: Added additional route views.py: Additional view for querying Yelp API rest_search.js: Javascript to detect clicks and call Python API

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 path("", views.restaurant_search, name="restaurant_search"),
+path("api_query/<location>/<rating>/<price>/", views.api_query, name="api-query")
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from main import models
+import random
+import requests
 from rest_framework import viewsets, decorators
 from rest_framework.response import Response
+from django.contrib.auth.decorators import login_required
 from main.serializers import UserSerializer, IngredientSerializer, RecipesSerializer
 
 
@@ -42,3 +45,22 @@ def index(response):
 
 def restaurant_search(response):
     return render(response, 'restaurant_search.html')
+
+@login_required
+def api_query(response, location, rating, price="1,2,3,4"):
+    API_KEY = "3ITPjXB1GPOj78Fag-o0LLQv2nOt7gmQWNjDJxqo7RK-HYmwVTyzm7F0MK7-Z6sPFmyaiEH5sgfU6JkrH4nNV06JHFTJ7GSPFlj7CdSDx12qPtaezp0-x01xJ9G9XnYx"
+    get_total = requests.get("https://api.yelp.com/v3/businesses/search", headers={"Authorization": "Bearer " + API_KEY}, params={'location': location, 'price': price, 'categories': 'restaurants,All', 'limit': 50})
+    offset = random.randint(1, float(get_total.json()["total"]) - 20)
+    print(offset)
+    print(float(get_total.json()["total"]))
+    new_dict = {
+        "total": 0,
+        "businesses": [],
+    }
+    response_list = requests.get("https://api.yelp.com/v3/businesses/search", headers={"Authorization": "Bearer " + API_KEY}, params={'location': location, 'price': price, 'offset': offset, 'categories': 'restaurants,All', 'limit': 50})
+    if (float(rating) != 0):
+        for i in range(0, len(response_list.json()["businesses"]) - 1):
+            if response_list.json()["businesses"][i]["rating"] >= float(rating):
+                new_dict['total'] += 1
+                new_dict["businesses"].append(response_list.json()["businesses"][i])
+    return JsonResponse(new_dict)
