426be46f9a9a49ccd3629197b650e77b7e3a2a95
eNobreg <ezra.nobrega@outlook.com>
2020-06-14 23:39:15 -0400

    [Fix] Added missing comma

--- main/urls.py
+++ main/urls.py
@@ -9,7 +9,7 @@ re_path(r'logout/$', views.LogoutView, name="logout_request"),
 path("profile/", views.profile, name='profile'),
 path("api_query/<location>/<rating>/<price>/", views.api_query, name="api-query"),
 path("save_favorite/<rest_id>/", views.save_favorite, name="save-favorite"),
-path("delete_favorite/<r_id>/", views.delete_favorite, name="delete-favorite")
+path("delete_favorite/<r_id>/", views.delete_favorite, name="delete-favorite"),
 path("", views.index, name="index"),
 path("restaurants", views.restaurant_search, name="restaurant_search"),
 ]
